3b245de4fc81eda6af706f343de642b55d9ba689
asyu <65544947+asyu00@users.noreply.github.com>
2024-09-21 12:09:32 +0800

    Revert "Created 1inch API class"

--- bot/src/util.py
+++ bot/src/util.py
@@ -6,13 +6,3 @@ def tuple_to_dict(tup, names: list[str]):
 def tuples_to_dicts(tups, names: list[str]):
     """ Convert list of tuples to list of dicts """
     return list(map(lambda tup: tuple_to_dict(tup, names), tups))
-
-
-def parse_decimal(value, decimal):
-    """ Parse bigint into human amount. """
-    return value / (10**decimal)
-
-
-def format_decimal(value, decimal):
-    """ format human amount into bigint. """
-    return int(value * (10**decimal))
